85b904a727101f3b61865e7a21a82add88ac62a7
George Pickering <29524044+geopic@users.noreply.github.com>
2020-02-25 13:55:13 +0000

    Amend types declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,37 @@
 declare module 'stormdb' {
-  namespace StormDB {
-    class StormDBClass {
-      constructor(engine: StormDB.LocalFileEngine, options?: object);
+  class StormDB {
+    constructor(engine: LocalFileEngine, options?: object);
 
-      static browserEngine(path: string, options?: object): BrowserEngine;
-      static localFileEngine(path: string, options?: object): LocalFileEngine;
-      default(defaultValue: object): StormDBClass;
-      length(): StormDBClass;
-      delete(): void;
-      push(value: any): void;
-      get(value: any): StormDBClass;
-      set(key: any, value: any): StormDBClass;
-      map<T>(func: (value: any, index?: number, array?: any[]) => T): StormDBClass;
-      sort<T>(func: (a: T, b: T) => number): StormDBClass;
-      filter<T, S extends T>(func: (value: T, index?: number, array?: T[]) => value is S): StormDBClass;
-      value(): any;
-      setValue(value: any, pointers: any[], setRecursively?: boolean): void;
-      save(): Promise<void> | null;
-    }
+    static browserEngine: typeof BrowserEngine;
+    static localFileEngine: typeof LocalFileEngine;
 
-    class LocalFileEngine {
-      constructor(path: string, options?: object);
-      init(): object;
-      read(): object;
-      write(data: any): Promise<void> | null;
-    }
-
-    class BrowserEngine {
-      constructor(path: string, options?: object);
-      init(): object;
-      read(): object;
-      write(data: any): Promise<void> | null;
-    }
+    default(defaultValue: object): StormDB;
+    length(): StormDB;
+    delete(): void;
+    push(value: any): void;
+    get(value: any): StormDB;
+    set(key: any, value: any): StormDB;
+    map<T>(func: (value: any, index?: number, array?: any[]) => T): StormDB;
+    sort<T>(func: (a: T, b: T) => number): StormDB;
+    filter<T, S extends T>(func: (value: T, index?: number, array?: T[]) => value is S): StormDB;
+    value(): any;
+    setValue(value: any, pointers: any[], setRecursively?: boolean): void;
+    save(): Promise<void> | null;
   }
 
-  export default StormDB.StormDBClass;
+  class LocalFileEngine {
+    constructor(path: string, options?: object);
+    init(): object;
+    read(): object;
+    write(data: any): Promise<void> | null;
+  }
+
+  class BrowserEngine {
+    constructor(path: string, options?: object);
+    init(): object;
+    read(): object;
+    write(data: any): Promise<void> | null;
+  }
+
+  export = StormDB;
 }
